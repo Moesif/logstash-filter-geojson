84226a214ebc4bd3a421389f20ad38ce7d41231e
Devendra <devendra@Devendras-MacBook-Pro.local>
2020-11-24 00:16:25 -0800

    support to parse geoJson fields from body

--- README.md
+++ README.md
@@ -1,71 +1,32 @@
 # logstash-filter-geojson
-[GeoJSON](http://geojson.org/) provides an elegant solution for encoding domain agnostic geographic data. 
-However, when inserting documents into ElasticSearch, the GeoJSON format is not the best fit. There are several problems
-* The GeoJSON format nests domain specific attributes under the `properties` element. ElasticSearch/kibana work best with flatter data structures.
-* The ElasticSearch [geo_point](https://www.elastic.co/guide/en/elasticsearch/reference/1.4/mapping-geo-point-type.html) is not compatiable with GeoJSON's point type.
-* The kibana [Tile Map](https://www.elastic.co/guide/en/kibana/current/tilemap.html) visualization utilizies the [Geohash](https://www.elastic.co/guide/en/elasticsearch/reference/current/search-aggregations-bucket-geohashgrid-aggregation.html) aggregration. The Geohash aggregration only supports geo_point fields.
 
-This Logstash filter transforms GeoJSON into a structure more suitable for the ElasticStack by
-* Flattening the GeoJSON data structure by unnesting all keys in the `properties` element.
-* Storing point geometries in a format that is supported by ElasticSearch's geo_point type.
-* Converting all non-point GeoJSON geometries into a center point to support Geohash aggregations.
+Parses event request.body and response.body jsons recursively and extracts geo_points to separate paths request.geo_fields.body
+and response.geo_fields.body.
 
-## Example
-Use this example logstash configuration...
+Geo_points are extracted matching below criteria
+- GeoJson format
+    - Geojson format consisting of type and coordinates. type can point, line, polygon, etc.
+    - For non single points like lines and polygons, centroid is calculated(averaging of coordinates in each dimension)
+    - For multipolygons, multi line strings, centroid is calculated for each polygon/line string and result will be array of geo_points
+- Any JSON Object containing the key sets (ignore case):
+     `{"lat", "lon"}` OR `{"latitude", "longitude"}` OR `{"lat", "lng"}` (Google format)
+- Key name of `location`, `latlng` (mapquest and google), `coordinates`, `position` or starts with `geo` AND the value is an array or string.
+    - If string value starts with "POINT", then parse as "POINT (-71.34 41.12)"
+    - Else parse as "lat, lon" if a string containing, Note the order.
+    - Else try parse as [lat, lon] if array.
+
+## Usage
 ```
 filter {
   geojson {  
   }
 }
-```
-
-...to convert GeoJSON documents like this..
-
-```
-{
-  "type": "Feature",
-  "geometry": {
-    "type": "Point",
-    "coordinates": [125.6, 10.1]
-  },
-  "properties": {
-    "name": "Dinagat Islands"
-  }
-}
-```
-
-...into documents like this...
-
-```
-{
-  "centroid": {
-    "lat" : 10.1,
-    "lon" : 125.6
-  },
-  "name": "Dinagat Islands"
-}
-```
-
-## Configuration Options
-| Configuration | Type | Default | Description |
-| ------------- |:----:|:-------:|:-----------:|
-| geometry_centroid_key | string | "centroid" | Specify event key containing geometry centroid  |
-| properties_dig_level | integer | 1 | Specify number of levels to flatten `properties`. Specify 0 to not flatten properties, -1 to flatten all levels |
-| properties_ignore_list | array of strings | [] | List of `properties` keys to ignore. Key will be ignored at all nested levels |
-
-## Build instructions
-
-### Set up env
-* Set up [development envirnoment](https://github.com/EagerELK/logstash-development-environment)
-* clone code base `git clone git@github.com:nreese/logstash-filter-geojson.git`
-* `cd logstash-filter-geojson`
-* `bundle install`
- 
-### Run tests
-* `bundle exec rspec`
-
+``` 
 ### Build plugin
 * `gem build logstash-filter-geojson.gemspec`
 
 ### Install plugin
-* `$LOGSTASH_HOME/bin/plugin install logstash-filter-geojson-0.1.0.gem`
+* `$LOGSTASH_HOME/bin/plugin install logstash-filter-geojson-1.0.0.gem`
+
+Extended from https://github.com/nreese/logstash-filter-geojson
+
--- lib/logstash/filters/geojson.rb
+++ lib/logstash/filters/geojson.rb
@@ -4,26 +4,17 @@ require "logstash/namespace"
 
 # Filter to dig out GeoJSON fields for easier usage in ElasticStack.
 class LogStash::Filters::GeoJSON < LogStash::Filters::Base
-  PROPERTIES_KEY="properties"
   GEOMETRY_KEY="geometry"
   GEOJSON_LAT_INDEX=1
   GEOGJSON_LON_INDEX=0
 
   config_name "geojson"
 
-  config :properties_dig_level, :validate => :number, :default => 1
-
-  config :properties_ignore_list, :validate => :array, :default => []
-
-  config :geometry_centroid_key, :validate => :string, :default => "centroid"
-
   public
   def register
-    @ignoreSet = @properties_ignore_list.to_set
   end # def register
 
   #Coords is an array of lon, lat pairs
-  public 
   def getCenter(coords)
     count = 0
     sumLat = 0
@@ -40,7 +31,6 @@ class LogStash::Filters::GeoJSON < LogStash::Filters::Base
     return center
   end
 
-  public
   def convertToGeopoint(geometry)
     centroid = nil
     if "Point".casecmp(geometry["type"]) == 0
@@ -74,50 +64,199 @@ class LogStash::Filters::GeoJSON < LogStash::Filters::Base
     return centroid
   end
 
-  public
-  def dig(k, v, nestLevel, digLevel, existingKeys)
-    @logger.debug("flattening, remaining levels: " + digLevel.to_s + ", key: " + k + ", value: " + v.to_s + ", type: " + v.class.to_s)
-    if (digLevel != 0) && (v.is_a? Hash)
-      nested = {}
-      allKeys = existingKeys.clone #keys passed in plus keys pulled from nested objects
-      v.each do |nestedKey, nestedVal|
-        if !@ignoreSet.include? nestedKey
-          subNested = dig(nestedKey, nestedVal, nestLevel+1, digLevel - 1, allKeys)
-          allKeys = allKeys.concat(subNested.keys)
-          nested = nested.merge(subNested)
+  # Function to parse body
+  def parse_body(event, object_field, fetch_body_field, parsed_body_field, org_id, app_id)
+    begin
+      # Empty hash to store geo fields
+      geo_fields = Hash.new
+      # Fetch the request/response body
+      fetched_body = event.get(fetch_body_field)
+      if !fetched_body.nil?
+        # Fetch the request/response object
+        req_resp_object = event.get(object_field)
+        # Fetch the content-type from headers
+        content_type = nil
+        if req_resp_object.key?('headers') && !req_resp_object['headers'].nil? && req_resp_object['headers'].key?('Content-Type')
+          content_type = req_resp_object['headers']['Content-Type']
+          @logger.debug('geoJsonParser: Content type -  ', content: content_type)
+
+          # Check if the content type is application/json and body is of type Hash or Array (Json)
+          if !content_type.nil? && (content_type.include? 'application/json') && (fetched_body.is_a? Hash or fetched_body.is_a? Array)
+            parse_json_body(event, fetched_body, geo_fields, parsed_body_field, org_id, app_id)
+          else
+            # Set the parsed body object to empty hash
+            @logger.debug('geoJsonParser: Content-type is not json so setting to empty array for orgId - ' << org_id << ' and appId - ' << app_id)
+            event.set(parsed_body_field, geo_fields)
+          end
+        elsif (fetched_body.is_a? Hash && !fetched_body.key?('_raw')) || (fetched_body.is_a? Array)
+          @logger.debug('geoJsonParser: Content-type header is not availabe and body is of type JSON so parsing body as JSON for orgId - ' << org_id << ' and appId - ' << app_id)
+          parse_json_body(event, fetched_body, geo_fields, parsed_body_field, org_id, app_id)
+        else
+          @logger.debug('geoJsonParser: Content-type header is not available and body is not of type JSON so setting to empty array for orgId - ' << org_id << ' and appId - ' << app_id)
+          event.set(parsed_body_field, geo_fields)
+        end
+      else
+        # Body is empty
+        @logger.debug('geoJsonParser: Body fetched from the event is empty for orgId - ' << org_id << ' and appId - ' << app_id)
+        event.set(parsed_body_field, geo_fields)
+      end
+    rescue StandardError => e
+      @logger.warn('geoJsonParser: error processing body for orgId - ' << org_id << ' and appId - ' << app_id << ' with exception - ', exception: e)
+      event.set(parsed_body_field, Hash.new)
+    end
+  end
+
+  # Function to parse json body
+  def parse_json_body(event, body, geo_fields, parsed_body_field, org_id, app_id)
+    begin
+      # Process the JSON body
+      process_body(body, geo_fields, org_id, app_id)
+      event.set(parsed_body_field, geo_fields)
+    rescue => e
+      @logger.warn('geoJsonParser: error parsing JSON body for orgId - ' << org_id << ' and appId - ' << app_id << ' with exception - ', exception: e)
+      event.set(parsed_body_field,  geo_fields)
+    end
+  end
+
+  # Function to process body
+  def process_body(body, geo_fields, org_id, app_id)
+    @logger.debug('geoJsonParser: process body data for orgId - ' << org_id << ' and appId - ' << app_id)
+    # extract the geo fields
+    extract_geo_fields(body, geo_fields)
+  end
+
+  # Function to extract geo fields
+  def extract_geo_fields(data, prefix= "", geo_fields)
+    # Add dot(.) to prefix in case of nested hash
+    prefixDot = !(prefix.empty? || prefix.nil?) ? prefix + '.' : ''
+    # Check if the data is of type hash
+    if data.is_a? Hash
+      geo_point = extract_geo_point(data)
+      add_to_geo_fields(geo_point, geo_fields)
+      data.each do |key, value|
+        if key
+          sub_hash = Hash.new
+          extract_geo_fields(value, prefixDot + key.to_s, sub_hash)
+          if !sub_hash.empty? && key
+            geo_fields[key.to_s] = sub_hash
+          end
         end
       end
-      return nested
+    # Check if the data is of type array
+    elsif data.is_a? Array
+      # For each element in data, recursive function to find the leaf node
+      data.each { |item| extract_geo_fields(item, prefixDot, geo_fields)}
+    end
+  end
+
+
+  def add_to_geo_fields(geo_point, geo_fields)
+    if !geo_point
+      return
+    end
+    key = "geo_point"
+    if geo_fields.key?(key) && (geo_fields[key].is_a? Array)
+      geo_fields[key].push(geo_point)
+    elsif geo_fields.key?(key)
+      geo_fields[key] = [geo_fields[key], geo_point]
     else
-      if existingKeys.include? k
-        return {k + "_level" + nestLevel.to_s => v}
-      else
-        return {k => v}
+      geo_fields[key] = geo_point
+    end
+  end
+
+  def extract_geo_point(input)
+    # transform keys to lowercase
+    input.transform_keys!(&:downcase)
+    # check for geometry
+    if input.key?("type") && input.key?("coordinates")
+      return convertToGeopoint(input)
+    # check for lat/lon
+    elsif input.key?("lat") && input.key?("lon")
+      return parse_lat_lon_from_array([input["lat"], input["lon"]])
+    # check for latitude/longitude
+    elsif input.key?("latitude") && input.key?("longitude")
+      return parse_lat_lon_from_array([input["latitude"], input["longitude"]])
+    # check for lat/lng
+    elsif input.key?("lat") && input.key?("lng")
+      return parse_lat_lon_from_array([input["lat"], input["lng"]])
+    elsif input.key?("location")
+      return parse_lat_lon(input["location"])
+    elsif input.key?("latlng")
+      return parse_lat_lon(input["latlng"])
+    elsif input.key?("coordinates")
+      return parse_lat_lon(input["coordinates"])
+    elsif input.key?("position")
+      return parse_lat_lon(input["position"])
+    else
+      input.each do |key, value|
+        if key.start_with?("geo")
+          point = parse_lat_lon(value)
+          if point
+            return point
+          end
+        end
       end
     end
+    return nil
+  rescue StandardError => e
+    @logger.warn('geoJsonParser: unable to extract geo_point', data: input, exception: e)
+    return nil
+  end
+
+  def parse_lat_lon(input)
+    if input.is_a? String
+      str_point = nil
+      if input.start_with?("POINT")
+       str_point = input.scan(/POINT\(([^>]*)\)/).last.first
+      else
+       str_point = input
+      end
+      coords = str_point.split(",")
+      return parse_lat_lon_from_array(coords)
+    elsif input.is_a? Array
+      return parse_lat_lon_from_array(input)
+    end
+  end
+
+  def valid_float(str)
+    !!Float(str) rescue false
+  end
+
+  def parse_lat_lon_from_array(input)
+    if input.length < 2
+      return nil
+    end
+    point = nil
+    if (input[0].is_a? Numeric) && (input[1].is_a? Numeric)
+      point = {
+        "lat" => input[0],
+        "lon" => input[1]
+      }
+    elsif (input[0].is_a? String) && (input[1].is_a? String) && valid_float(input[0]) && valid_float(input[1])
+      point = {
+        "lat" => input[0].to_f,
+        "lon" => input[1].to_f
+      }
+    end
+    return point
   end
 
   public
   def filter(event)
-    if event[PROPERTIES_KEY] && @properties_dig_level != 0
-      props = dig(
-        "props", 
-        event[PROPERTIES_KEY], 
-        0,
-        @properties_dig_level,
-        event.to_hash_with_metadata.keys)
-      @logger.debug("flattened properties: " + props.to_s)
-      props.each do |k, v|
-        event[k] = v
-      end
-      event.remove(PROPERTIES_KEY)
-    end
-
-    if event[GEOMETRY_KEY]
-      event[@geometry_centroid_key] = convertToGeopoint(event[GEOMETRY_KEY])
-    end
-    
-    # filter_matched should go in the last line of our successful code
+    # Fetch Event OrgId
+    org_id = event.get("[org_id]")
+    # Fetch Event AppId
+    app_id = event.get("[app_id]")
+    # Request Body
+    @logger.debug('geoJsonParser: Begin parsing for request body for orgId - ' << org_id << ' and appId - ' << app_id)
+    parse_body(event, "[request]",  "[request][body]", "[request][geo_fields][body]", org_id, app_id)
+    # Response Body
+    @logger.debug('geoJsonParser: Begin parsing response body for orgId - ' << org_id << ' and appId - ' << app_id)
+    parse_body(event, "[response]",  "[response][body]", "[response][geo_fields][body]", org_id, app_id)
+    # Filter match event
     filter_matched(event)
+  rescue StandardError => e
+     @logger.warn('geoJsonParser: error while parsing geoJson', exception: e)
   end # def filter
+
 end # class LogStash::Filters::GeoJSON
